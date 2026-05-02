c6f2c3f5c82e145ddfb6b97cccfb5954ac08e1a8
Dorotea Monaco <134923734+doroteaMonaco@users.noreply.github.com>
2026-05-02 15:02:41 +0200

    Add implementation for palindrome checking using two-pointer technique

---
+++ NeetCode/Two Pointers/IsPalindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_s = ""
+
+        s = s.lower()
+        for c in s:
+            if c.isalnum() and not c.isspace():
+                new_s += c
+        
+        left, right = 0, len(new_s) - 1
+        
+        while left < right:
+            if new_s[left] != new_s[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
