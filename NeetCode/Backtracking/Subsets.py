1bd458802ba45181bf35da328f6e655a460d8be5
Dorotea Monaco <134923734+doroteaMonaco@users.noreply.github.com>
2026-05-30 12:18:09 +0200

    Implementation of an algorithm to fin all the subsets of an array including the empty solution done by the powerset algorithm

---
+++ NeetCode/Backtracking/Subsets.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        sol = [0] * len(nums)
+        def powerset_bitmask(nums, pos, sol, n):
+            if pos == n:
+                powerset = [nums[i] for i in range(n) if sol[i] == 1]
+                res.append(powerset)
+                return 1
+
+
+            sol[pos] = 0
+            count = powerset_bitmask(nums, pos + 1, sol, n)
+            sol[pos] = 1
+            count += powerset_bitmask(nums, pos + 1, sol, n)
+            
+
+            return count
+        powerset_bitmask(nums, 0, sol, len(nums))
+        return res
+
+#Time complexity: O(n * 2^n) where n is the length of the input list. This is because there are 2^n subsets and generating each subset takes O(n) time in the worst case.
+#Space complexity: O(n) for the recursion stack and the temporary solution list. The output list will take O(2^n) space in the worst case, but we typically don't count the output space when analyzing space complexity.
+
