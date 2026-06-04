6ed0b9774295ca3d2a6e11b3ae96d8fff5143303
Dorotea Monaco <134923734+doroteaMonaco@users.noreply.github.com>
2026-06-04 20:24:20 +0200

    Linear Regression in FP

---
+++ Machine Learning/Math/Linear Regression (FP).py
@@ -0,0 +1,16 @@
+import numpy as np
+from numpy.typing import NDArray
+
+class Solution:
+
+    def get_model_prediction(self, X: NDArray[np.float64], weights: NDArray[np.float64]) -> NDArray[np.float64]:
+        # X is (n, m), weights is (m,) -> return (n,) predictions
+        # Round to 5 decimal places
+        preds = np.matmul(X, weights)
+        return np.round(preds, 5) # np.round because it is an np.ndarray, not a single float
+
+    def get_error(self, model_prediction: NDArray[np.float64], ground_truth: NDArray[np.float64]) -> float:
+        # Compute mean squared error between predictions and ground truth
+        # Round to 5 decimal places
+        mse = np.mean((model_prediction - ground_truth)**2)
+        return round(mse, 5)
