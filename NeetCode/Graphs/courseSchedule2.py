d531fd3941d7150f6efd5d082b54cd6b5a1faf9b
Dorotea Monaco <134923734+doroteaMonaco@users.noreply.github.com>
2026-07-01 20:06:48 +0200

    implement DFS approach for course scheduling problem

--- NeetCode/Graphs/courseSchedule2.py
+++ NeetCode/Graphs/courseSchedule2.py
@@ -31,3 +31,38 @@ class Solution:
     
 #Time complexity: O(V + E) where V is the number of vertices and E is the number of edges in the graph
 #Space complexity: O(V + E) for the graph and O(V) for the queue
+
+#Other solutions
+#DFS
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        prereq = {c: [] for c in range(numCourses)}
+        for crs, pre in prerequisites:
+            prereq[crs].append(pre)
+
+        output = []
+        visit, cycle = set(), set()
+
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            if crs in visit:
+                return True
+
+            cycle.add(crs)
+            for pre in prereq[crs]:
+                if dfs(pre) == False:
+                    return False
+            cycle.remove(crs)
+            visit.add(crs)
+            output.append(crs)
+            return True
+
+        for c in range(numCourses):
+            if dfs(c) == False:
+                return []
+        return output
+
+#Time complexity: O(V + E) where V is the number of vertices and E is the number of edges in the graph
+#Space complexity: O(V + E) for the graph and O(V) for the recursion
+
